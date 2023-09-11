3246114f3c65c64a1aea10f7865f1a03cc8987bd
Shivangi-Agrawal797 <142230384+Shivangi-Agrawal797@users.noreply.github.com>
2023-09-12 01:49:21 +0530

    Update Trade.java

--- FXTRADING/src/main/java/com/FXTRADING/FXTRADING/Model/Trade.java
+++ FXTRADING/src/main/java/com/FXTRADING/FXTRADING/Model/Trade.java
@@ -3,7 +3,9 @@ package com.FXTRADING.FXTRADING.Model;
 import io.micrometer.common.lang.NonNull;
 
 public class Trade {
-
+    /*
+     * attributes
+     */
 	private static int tradeCounter = 1;
 	private double rate = 66.0;
 	private int tradeNumber;
@@ -11,70 +13,61 @@ public class Trade {
 	private String currencyPair;
 	private double amount;
 	
-	
-	Trade(){
-		
+	//Default constructor
+	public Trade () {
 	}
-
-    public Trade(String customerName, String currencyPair, double amount) {
+	
+	//Parameterized constructor
+	public Trade(String customerName, String currencyPair, double amount) {
 		super();
 		this.tradeNumber = tradeCounter++;
 		this.customerName = customerName;
 		this.currencyPair = currencyPair;
 		this.amount = amount;
 	}
-
-    
-    @NonNull
+	
+	
+	//Getter methods for retrieving field values
 	public double getRate() {
 		return rate;
 	}
-
-	public void setRate(double rate) {
-		this.rate = rate;
-	}
-
 	public int getTradeNumber() {
 		return tradeNumber;
 	}
-
-	public void setTradeNumber(int tradeNumber) {
-		this.tradeNumber = tradeNumber;
-	}
-
-	public  String getCustomerName() {
+	public String getCustomerName() {
 		return customerName;
 	}
-
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
-
 	public String getCurrencyPair() {
 		return currencyPair;
 	}
-
-	public void setCurrencyPair(String currencyPair) {
-		this.currencyPair = currencyPair;
-	}
-
 	public double getAmount() {
 		return amount;
 	}
-
+	
+	// Setter method for updating field
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+	public void setTradeNumber(int tradeNumber) {
+		this.tradeNumber = tradeNumber;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public void setCurrencyPair(String currencyPair) {
+		this.currencyPair = currencyPair;
+	}
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
-
 	
+	//toString method overriding
 	@Override
 	public String toString() {
-		return "Trade [rate=" + rate + ", tradeNumber=" + tradeNumber + ", customerName=" + customerName
-				+ ", currencyPair=" + currencyPair + ", amount=" + amount + "]";
-	}
-	
-
-	
-    
-	
+		return "Trade [rate=" + rate + ", "
+				+ "tradeNumber=" + tradeNumber + ", "
+				+ "customerName=" + customerName
+				+ ", currencyPair=" + currencyPair 
+				+ ", amount=" + amount + "]";
+	}	
 }
