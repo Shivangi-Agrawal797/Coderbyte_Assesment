2ab7ad5d01f0344dea58daf54fc674e7b8015e10
Shivangi-Agrawal797 <142230384+Shivangi-Agrawal797@users.noreply.github.com>
2023-09-12 01:58:02 +0530

    Update FormatFxTrading.java

--- FXTRADING/src/main/java/com/FXTRADING/FXTRADING/Services/FormatFxTrading.java
+++ FXTRADING/src/main/java/com/FXTRADING/FXTRADING/Services/FormatFxTrading.java
@@ -23,31 +23,21 @@ public class FormatFxTrading
 		Map<String , String> userError = new TreeMap<>();
 		try {
 		if(trade.getCustomerName().isBlank())
-		
 			userError.put("Customer-Name", "Customer name cannot be empty");
-		
 		if(trade.getAmount()<=0)
-		
 			userError.put("USD-Amount", "Amount cannot be Negative");
-		
 		if(!trade.getCurrencyPair().equals("USDINR"))
-		
 			userError.put("Currency-Pair", "Currency pair should be 'USDINR' only!");
-		
-		
 		if(userError.isEmpty())
-		                    
-	    indianAmount=trade.getAmount()*trade.getRate();
+	        indianAmount=trade.getAmount()*trade.getRate();
 		trade.setAmount(indianAmount);
-		
 		Trade trade1 = new Trade(trade.getCustomerName(), trade.getCurrencyPair(), trade.getAmount());
 		userError.put("message", "Trade for " + trade.getCurrencyPair()+ "has been booked with rate " 
 		              + trade.getRate() + "The amount of Rs " + trade.getAmount()
 		              + "will be transferred in 2 working days to " + trade.getCustomerName());
-          
+			
 		bookedTrade.add(trade1);
-		return trade;
-		             
+		return trade;        
 		}
 		catch(Exception e){
 			return Collections.singletonMap("error", "Kindly check request body");
