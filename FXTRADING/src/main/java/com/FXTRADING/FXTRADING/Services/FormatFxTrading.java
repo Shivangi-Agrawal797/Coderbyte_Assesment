734e347a5f7d2fc65ca983e6c972b115fd9e600b
Shivangi-Agrawal797 <142230384+Shivangi-Agrawal797@users.noreply.github.com>
2023-09-12 01:53:20 +0530

    Update FormatFxTrading.java

--- FXTRADING/src/main/java/com/FXTRADING/FXTRADING/Services/FormatFxTrading.java
+++ FXTRADING/src/main/java/com/FXTRADING/FXTRADING/Services/FormatFxTrading.java
@@ -1,58 +1,61 @@
-package com.FXTRADING.FXTRADING.Services;
+package com.Fxtrading.FXTrading.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 import org.springframework.stereotype.Service;
 
-import com.FXTRADING.FXTRADING.Model.Trade;
+import com.Fxtrading.FXTrading.Model.Trade;
 
 @Service
-public class FormatFxTrading{
-	
+public class FormatFxTrading 
+    {
+	//list for string trades
 	private static List<Trade> bookedTrade = new ArrayList<>();
-
-	public static Object doTrade(Trade trade) {
-		
+	
+	//Validation method
+	public static Object dsoTrade(Trade trade) 
+	{
+		double indianAmount = 0;
 		Map<String , String> userError = new TreeMap<>();
-		
+		try {
 		if(trade.getCustomerName().isBlank())
-		{
+		
 			userError.put("Customer-Name", "Customer name cannot be empty");
-		}
+		
 		if(trade.getAmount()<=0)
-		{
+		
 			userError.put("USD-Amount", "Amount cannot be Negative");
-		}
+		
 		if(!trade.getCurrencyPair().equals("USDINR"))
-		{
-			userError.put("Currency-Pair", "Currency pair is not USDINR");
-		}
+		
+			userError.put("Currency-Pair", "Currency pair should be 'USDINR' only!");
+		
 		
 		if(userError.isEmpty())
-		{
-			double indianAmount=trade.getAmount()*trade.getRate();
-			trade.setAmount(indianAmount);
-			
-			Trade trade1 = new Trade(trade.getCustomerName(), trade.getCurrencyPair(), trade.getAmount());
-			userError.put("message", "Trade for " + trade.getCurrencyPair()+ "has been booked with rate " + trade.getRate() + "The amount of Rs " + trade.getAmount()+ "will be transferred in 2 working days to " + trade.getCustomerName());
-	          
-			bookedTrade.add(trade1);
-			return trade;
-		}
-		else {
-			return userError;
-		}
+		                    
+	    indianAmount=trade.getAmount()*trade.getRate();
+		trade.setAmount(indianAmount);
 		
-	}
-
-
-	public List<Trade> getTradeList() {
-		
-		return this.bookedTrade;
-	}
+		Trade trade1 = new Trade(trade.getCustomerName(), trade.getCurrencyPair(), trade.getAmount());
+		userError.put("message", "Trade for " + trade.getCurrencyPair()+ "has been booked with rate " 
+		              + trade.getRate() + "The amount of Rs " + trade.getAmount()
+		              + "will be transferred in 2 working days to " + trade.getCustomerName());
+          
+		bookedTrade.add(trade1);
+		return trade;
+		             
+		}
+		catch(Exception e){
+			return Collections.singletonMap("error", "Kindly check request body");
+		}
+	    }
 	
+	    public List<Trade> getTradeList() {
+		return this.bookedTrade;
+	    }
 	
 }
